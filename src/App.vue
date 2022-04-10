f866bb3ae146457c8d63c7953f329989af4056f3
OziOcb <pozyzniewski@yahoo.com>
2022-04-10 23:02:49 +0100

    Create TheHeader.vue / Remove some unused scripts and fonts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import TheHeader from "./components/TheHeader.vue";
 import TheHero from "./components/TheHero.vue";
 </script>
 
 <template>
+  <TheHeader />
   <TheHero />
 </template>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="site-header site-header--menu-right landing-13-menu site-header--absolute site-header--sticky">
+    <div class="container">
+      <nav class="navbar site-navbar">
+        <!-- Brand Logo-->
+        <div class="brand-logo">
+          <a href="#">
+            <img src="src/assets/image/logo/logo_veriffica--primary.svg" alt="" />
+          </a>
+        </div>
+      </nav>
+    </div></header>
+</template>
