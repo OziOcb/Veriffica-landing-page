d90d1a3f14497a73c5cfc3e013fb4d9f2203ed31
OziOcb <pozyzniewski@yahoo.com>
2022-04-10 22:39:05 +0100

    Import all assets and create TheHero component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import TheHero from "./components/TheHero.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo_veriffica--primary.svg" />
-  <HelloWorld msg="Website in build" />
+  <TheHero />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/bootstrap.css";
+@import "./assets/fonts/icon-font/css/style.css";
+@import "./assets/fonts/typography-font/typo.css";
+@import "./assets/fonts/fontawesome-5/css/all.css";
+
+@import url('https://fonts.googleapis.com/css2?family=Karla:wght@300;400;500;600;700;800&display=swap');
+/* @import url('https://fonts.googleapis.com/css2?family=Gothic+A1:wght@400;500;700;900&display=swap'); */
+/* @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700;800;900&display=swap'); */
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;800;900&display=swap');
+/* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap'); */
+
+@import "./plugins/aos/aos.min.css";
+@import "./assets/css/main.css";
 </style>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,12 +1,29 @@
 <template>
-  <h1>elo</h1>
+  <div class="hero-area-l-13 position-relative overflow-hidden">
+    <div class="container">
+      <div class="row position-relative justify-content-center">
+        <div class="col-xl-6 col-lg-6 col-sm-9" data-aos="fade-right" data-aos-duration="800" data-aos-once="true">
+          <div class="banner-image-l-13">
+            <img src="src/assets/image/l3/iphone-img.svg" alt="" class="w-100">
+          </div>
+        </div>
+
+        <div class="offset-xl-1 col-xl-5 col-lg-6 col-md-8 col-sm-9" data-aos="fade-left" data-aos-duration="800"
+          data-aos-once="true">
+          <div class="content">
+            <h1>Get millions of downloads!</h1>
+
+            <p>Create a landing page for your app with Shades to get better results.</p>
+
+            <div class="apps-btn">
+              <a href="#" class="app-store"><img src="src/assets/image/l3/app-store.png" alt=""></a>
+              <a href="#" class="app-store"><img src="src/assets/image/l3/google-play.png" alt=""></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-shape-13"></div>
+  </div>
 </template>
-
-<script setup>
-</script>
-
-<style lang="scss" scoped>
-a {
-  color: #42b983;
-}
-</style>
