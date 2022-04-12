93fb0001de2039c4e2722a4691b3f5fa02a34598
OziOcb <pozyzniewski@yahoo.com>
2022-04-12 21:15:54 +0100

    Set Hero's text color to white (only on desktop)

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -2,22 +2,42 @@
   <div class="hero-area-l-13 position-relative overflow-hidden">
     <div class="container">
       <div class="row position-relative justify-content-center">
-        <div class="col-xl-6 col-lg-6 col-sm-9" data-aos="fade-right" data-aos-duration="800" data-aos-once="true">
+        <div
+          class="col-xl-6 col-lg-6 col-sm-9"
+          data-aos="fade-right"
+          data-aos-duration="800"
+          data-aos-once="true"
+        >
           <div class="banner-image-l-13">
-            <img src="src/assets/image/l3/iphone-img.svg" alt="" class="w-100">
+            <img
+              src="src/assets/image/l3/iphone-img.svg"
+              alt=""
+              class="w-100"
+            />
           </div>
         </div>
 
-        <div class="offset-xl-1 col-xl-5 col-lg-6 col-md-8 col-sm-9" data-aos="fade-left" data-aos-duration="800"
-          data-aos-once="true">
-          <div class="content">
+        <div
+          class="offset-xl-1 col-xl-5 col-lg-6 col-md-8 col-sm-9"
+          data-aos="fade-left"
+          data-aos-duration="800"
+          data-aos-once="true"
+        >
+          <div class="content ozi-hero__content">
             <h1>Get millions of downloads!</h1>
 
-            <p>Create a landing page for your app with Shades to get better results.</p>
+            <p>
+              Create a landing page for your app with Shades to get better
+              results.
+            </p>
 
             <div class="apps-btn">
-              <a href="#" class="app-store"><img src="src/assets/image/l3/app-store.png" alt=""></a>
-              <a href="#" class="app-store"><img src="src/assets/image/l3/google-play.png" alt=""></a>
+              <a href="#" class="app-store"
+                ><img src="src/assets/image/l3/app-store.png" alt=""
+              /></a>
+              <a href="#" class="app-store"
+                ><img src="src/assets/image/l3/google-play.png" alt=""
+              /></a>
             </div>
           </div>
         </div>
@@ -27,3 +47,11 @@
     <div class="bg-shape-13"></div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.ozi-hero__content * {
+  @media (min-width: 992px) {
+    color: #fbfbfb !important;
+  }
+}
+</style>
