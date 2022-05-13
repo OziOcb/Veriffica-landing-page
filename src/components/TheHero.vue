6bfdc8089744d0a51e5e7463c3126a3ef4c0879c
OziOcb <pozyzniewski@yahoo.com>
2022-05-13 16:19:37 +0100

    Add AppStore link / Add extra description in the Hero

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -2,37 +2,30 @@
   <div class="hero-area-l-13 position-relative overflow-hidden">
     <div class="container">
       <div class="row position-relative justify-content-center">
-        <aos-vue
-          class="col-xl-6 col-lg-6 col-sm-9"
-          animation="fade-right"
-          :duration="800"
-          :once="true"
-        >
+        <aos-vue class="col-xl-6 col-lg-6 col-sm-9" animation="fade-right" :duration="800" :once="true">
           <div class="banner-image-l-13">
             <img src="@/assets/image/l3/hero-image.png" alt="" class="w-100" />
           </div>
         </aos-vue>
 
-        <aos-vue
-          class="
+        <aos-vue class="
             ozi-contentWrapper
             offset-xl-1
             col-xl-5 col-lg-6 col-md-8 col-sm-9
-          "
-          animation="fade-left"
-          :duration="800"
-          :once="true"
-        >
-          <FormWaitingList class="ozi-form" />
-          <!-- <div class="ozi-appsBtn apps-btn">
-            <a href="#" class="app-store">
-              <img src="@/assets/image/l3/app-store.png" alt="" />
+          " animation="fade-left" :duration="800" :once="true">
+
+          <div class="ozi-appsBtn apps-btn">
+            <a href="https://apps.apple.com/gb/app/veriffica-car-check/id1589604742" class="app-store"
+              rel="noreferrer noopener" target="_blank">
+              <img src="@/assets/image/l3/apple-app-store--black.svg" alt="" />
             </a>
 
-            <a href="#" class="app-store">
+            <!-- <a href="#" class="app-store">
               <img src="@/assets/image/l3/google-play.png" alt="" />
-            </a>
-          </div> -->
+            </a> -->
+          </div>
+
+          <FormWaitingList class="ozi-form" />
 
           <div class="content ozi-hero__content">
             <h1>Be sure!</h1>
@@ -40,6 +33,13 @@
             <p>
               Check all the most important elements of the vehicle before making
               a decision to buy it.
+            </p>
+
+            <p>
+              The application focuses mainly on the process that takes place during the inspection of the car. So step
+              by step, it tells the user what to check. The user has to answer with "Yes", "No", or "I don't know".
+              After answering all the questions, he gets the result in%. The higher the score, the better the car should
+              theoretically be.
             </p>
           </div>
         </aos-vue>
@@ -63,15 +63,39 @@ import FormWaitingList from "../components/FormWaitingList.vue";
 .ozi-form,
 .ozi-appsBtn {
   margin-top: 40px;
+
   @media (min-width: 992px) {
     margin-top: 0;
     order: 1;
   }
 }
 
-.ozi-hero__content * {
+.ozi-appsBtn {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px !important;
+
   @media (min-width: 992px) {
-    color: #fbfbfb !important;
+    margin-top: 0px !important;
+    margin-bottom: 20px !important;
+    justify-content: flex-start;
+  }
+}
+
+.app-store {
+  width: 100%;
+  max-width: 200px;
+}
+
+.ozi-hero__content {
+  p {
+    font-size: 18px !important;
+  }
+
+  * {
+    @media (min-width: 992px) {
+      color: #fbfbfb !important;
+    }
   }
 }
 </style>
